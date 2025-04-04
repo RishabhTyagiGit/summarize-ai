3624fa021ee69823954046c23616307eb4b19a0d
RishabhTyagiGit <mailtorishabhtyagi@gmail.com>
2025-04-04 16:40:55 +0530

    Footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-6 flex justify-center items-center bg-gray-100">
+      Designed and coded with ❤️️ by Rishabh Tyagi
+    </footer>
+  );
 };
 
 export default Footer;
