d9d1113c2ac59ffddfc17133a8786378490d9701
RishabhTyagiGit <mailtorishabhtyagi@gmail.com>
2025-04-06 20:33:48 +0530

    Langchain

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -4,6 +4,7 @@ import UploadFormInput from "@/components/upload/upload-form-input";
 import { z } from "zod";
 import { useUploadThing } from "@/utils/uploadthing";
 import { toast } from "sonner";
+import { generatePdfSummary } from "@/actions/upload-actions";
 
 const schema = z.object({
   file: z
@@ -55,6 +56,8 @@ const UploadForm = () => {
     }
     toast("Hang tight! Our AI is reading through your document!");
     // parse the pdf using lang chain
+    const summary = await generatePdfSummary(resp);
+    console.log(summary);
     // summarize the pdf using AI
     // save the summary to the database
     // redirect to the [id] summary page
---
+++ lib/langchain.ts
@@ -0,0 +1,13 @@
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
+export async function fetchAndExtractPdfText(fileUrl: string) {
+  const response = await fetch(fileUrl);
+  const blob = await response.blob();
+
+  const loader = new PDFLoader(blob);
+
+  const docs = await loader.load();
+
+  // combine all pages
+  return docs.map((doc) => doc.pageContent).join("\n");
+}
